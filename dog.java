9f055ded953065cb48c8821539e4ea45f6d21f07
Xi Ling <xxxlhaus@gmail.com>
2023-06-15 16:18:18 -0500

    v1 : create dog class

---
+++ dog.java
@@ -0,0 +1,19 @@
+public class dog{
+	public int weight;
+	public String color;
+
+	public static void main (String[] args) {
+
+	}
+
+
+	public static void bark (weight) {
+		if (weight > 20) {
+			System.out.println("bark");
+		}
+
+		else {
+			System.out.println("yinyin");
+		}
+	}
+}
